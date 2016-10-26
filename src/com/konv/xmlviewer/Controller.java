ac6876b750694d9170c16e5fb10d61d05c698e5d
konv <konvdev@gmail.com>
2016-10-26 22:02:39 +0300

    Define classes

---
+++ src/com/konv/xmlviewer/Controller.java
@@ -0,0 +1,15 @@
+package com.konv.xmlviewer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class Controller {
+
+    @FXML
+    private TableView mTableView;
+
+    @FXML
+    private void initialize() {
+
+    }
+}
--- src/com/konv/xmlviewer/Main.java
+++ src/com/konv/xmlviewer/Main.java
@@ -1,13 +1,19 @@
 package com.konv.xmlviewer;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("XML Viewer");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
